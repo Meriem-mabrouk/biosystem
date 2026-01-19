27f1b4d36cce1e8016913bb3cd8987c7b1adb4a2
meriem mabrouk <34204557+Meriem-mabrouk@users.noreply.github.com>
2026-01-19 21:03:59 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -3,8 +3,6 @@
             <p>🌿 BioSystem AI - © 2026</p>
             <p style="opacity: 0.6; font-size: 0.8rem;">Innovation durable pour entreprises modernes.</p>
         </div>
-        <script src="https://widgetiframeconversell.firebaseapp.com/index.js"></script>
-    <chat-widget chatbotid='b9e55d86-598c-4b19-8a77-946dcb4055aa' mode='widget'></chat-widget>
-    </footer>
+        </footer>
 </body>
 </html>
